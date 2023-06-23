2c437a9db63eaf169402688fbc5ea76e70d01c8b
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-06-23 12:05:56 -0700

    revert contact form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'),],
+  plugins: [require('@tailwindcss/typography')],
   darkMode: 'class',
 };
