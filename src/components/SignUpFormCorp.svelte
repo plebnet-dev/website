b265b0d8b75a4681933b67ec7e1387f86c1c5125
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-06-22 18:25:12 -0700

    adjusting env vars

--- src/components/SignUpFormCorp.svelte
+++ src/components/SignUpFormCorp.svelte
@@ -1,9 +1,7 @@
 <script>
   import { createClient } from '@supabase/supabase-js';
   import { fade } from 'svelte/transition';
-
-  const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-  const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY;
+  import { supabaseUrl, supabaseKey } from '../env.js';
 
   const supabase = createClient(supabaseUrl, supabaseKey);
 
