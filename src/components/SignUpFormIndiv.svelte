fd724d12fa70063536cf5109ea82dac86a065bad
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-06-22 18:34:17 -0700

    adjusting env vars

--- src/components/SignUpFormIndiv.svelte
+++ src/components/SignUpFormIndiv.svelte
@@ -3,8 +3,8 @@
   import { fade } from 'svelte/transition';
 
 
-  const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-  const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = Astro?.env.PUBLIC_SUPABASE_URL;
+const supabaseKey = Astro?.env.PUBLIC_SUPABASE_KEY;
 
 
 const supabase = createClient(supabaseUrl, supabaseKey);
