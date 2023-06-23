9b0a976239387083ef33d2d702f3a43466eb6bdc
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-06-23 12:05:56 -0700

    revert contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
@@ -59,7 +58,6 @@ export default defineConfig({
   }), svelte()],
 
   integrations: [
-    svelte(),
     tailwind({
       config: {
         applyBaseStyles: false,
