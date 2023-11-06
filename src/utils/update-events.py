1044b67b64a340919aacdb82de999e7fd31f1468
Brandon <brandon@saucy.tech>
2023-11-06 18:16:09 -0500

    Refactor event processing Remove discord.py, modularize functions, use direct Discord API calls

--- src/utils/update-events.py
+++ src/utils/update-events.py
@@ -1,105 +1,150 @@
 import os
-import discord
-from github import Github, GithubException
-from datetime import datetime
+import requests
 import re
+from datetime import datetime, timezone
 
 # Load environment variables from .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-# Load tokens and repo details from environment variables
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-GITHUB_TOKEN = os.getenv('GH_PAT')
-REPO_NAME = ""
+# Configuration
+BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+GUILD_ID = os.getenv('DISCORD_GUILD_ID')
+PAT = os.getenv('GH_PAT')  # Personal Access Token for GitHub
+REPO = os.getenv('REPO_NAME')
 FILE_PATH = 'src/content/post/events.md'
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-intents.presences = True
-
-client = discord.Client(intents=intents)
-
-def update_markdown(events):
+# Function to fetch channel name for events from Discord
+def fetch_discord_channel_name(channel_id, bot_token):
     try:
-        # Authenticate with GitHub
-        g = Github(GITHUB_TOKEN)
-        # Get the repository
-        repo = g.get_repo(REPO_NAME)
-        # Get the file contents
-        contents = repo.get_contents(FILE_PATH, ref="main")
-        content = contents.decoded_content.decode('utf-8')
+        url = f"https://discord.com/api/v9/channels/{channel_id}"
+        headers = {
+            "Authorization": f"Bot {bot_token}"
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # This will raise an exception for HTTP error responses
+        channel = response.json()
+        return channel.get('name', 'Unknown Channel')  # Return the name of the channel or 'Unknown Channel' if not found
+    except Exception as e:
+        print(f"Error fetching channel name: {e}")
+        return 'Unknown Channel'  # Return a default value in case of any error
 
-        # Define a pattern to match event sections
-        pattern = r'(#{1,2} [^\n]*)\n(?:[^#]*\n)*'
-        matches = re.findall(pattern, content, re.DOTALL)
 
-        # Parse events
-        existing_events = []
-        current_section = None
-        for match in matches:
-            lines = match.strip().split('\n')
-            section = lines[0]
-            if section.startswith('# Upcoming Events'):
-                current_section = 'upcoming'
-            elif section.startswith('# Past Events'):
-                current_section = 'past'
-            elif section.startswith('##'):
-                name = lines[0][3:].strip()
-                date_match = re.search(r'Date: (.+)', '\n'.join(lines))
-                description_match = re.search(r'Description: (.+)', '\n'.join(lines))
-                channel_match = re.search(r'Channel: (.+)', '\n'.join(lines))
+# Function to fetch events from Discord
+def fetch_discord_events(guild_id, bot_token):
+    url = f"https://discord.com/api/v9/guilds/{guild_id}/scheduled-events"
+    headers = {
+        "Authorization": f"Bot {bot_token}"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        events = response.json()
+        return process_discord_events(events, bot_token)
+    except requests.RequestException as e:
+        print(f"Error fetching events: {e}")
+        return []
 
-                if date_match and description_match and channel_match:
-                    date = datetime.strptime(date_match.group(1), '%Y-%m-%d').date()
-                    description = description_match.group(1)
-                    channel = channel_match.group(1)
-                    existing_events.append({'name': name, 'date': date, 'description': description, 'channel': channel, 'section': current_section})
-                else:
-                    print(f"Error parsing event details for section: {section}")
+# Process and return events in a structured format
+def process_discord_events(events, bot_token):
+    structured_events = []
+    for event in events:
+        if event['status'] == 1:  # 1 is for SCHEDULED events
+            start_time = datetime.fromisoformat(event['scheduled_start_time'])
+            # Ensure start_time is aware by assigning UTC timezone if it's not set
+            if start_time.tzinfo is None:
+                start_time = start_time.replace(tzinfo=timezone.utc)
+            channel_id = event.get('channel_id')
+            channel_name = fetch_discord_channel_name(channel_id, bot_token) if channel_id else 'Unknown Channel'
+            print(f"Event: {event['name']}, Channel ID: {channel_id}, Channel Name: {channel_name}")  # Debug print
+            structured_events.append({
+                "name": event['name'],
+                "date": start_time.date(),
+                "start_time": start_time,
+                "end_time": event.get('scheduled_end_time'),
+                "description": event.get('description', 'No description provided.'),
+                "channel_name": channel_name,
+                "section": 'upcoming' if start_time >= datetime.now(timezone.utc) else 'past'
+            })
+    return structured_events
 
-        # Merge new events with existing events
-        for new_event in events:
-            # Determine if the event is upcoming or past based on the current date
-            new_event['section'] = 'upcoming' if new_event['date'] >= datetime.now().date() else 'past'
-            if new_event not in existing_events:
-                existing_events.append(new_event)
 
-        # Sort events by date
-        existing_events.sort(key=lambda x: x['date'])
+# Function to parse markdown content and extract events
+def parse_markdown(content):
+    frontmatter, event_content = content.split('---\n', 2)[1:3]
+    current_datetime = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    frontmatter = re.sub(r'publishDate:.*', f'publishDate: {current_datetime}', frontmatter)
+    pattern = r'(## [^\n]+)\n\nDate: ([^\n]+)\n\nDescription:\n([^\n]+(?:\n\n[^\n]+)*)\n\nLocation: ([^\n]+)'
+    matches = re.findall(pattern, event_content, re.DOTALL)
+    existing_events = []
+    for match in matches:
+        name, date_str, description, location = match
+        date = datetime.strptime(date_str, '%b %d, %Y').date()
+        existing_events.append({
+            'name': name.strip('# '),
+            'date': date,
+            'description': description.strip(),
+            'location': location,
+            'section': 'upcoming' if date >= datetime.now().date() else 'past'
+        })
+    return frontmatter, existing_events
 
-        # Generate updated content with frontmatter
-        updated_content = '---\ntitle: "Events"\nimage: "/src/content/post/_images/events.jpg"\n---\n\n# Upcoming Events\n\n'
-        updated_content += ''.join(f"## {event['name']}\n\nDate: {event['date'].strftime('%Y-%m-%d')}\nDescription: {event['description']}\nChannel: {event['channel']}\n\n" for event in existing_events if event['section'] == 'upcoming')
-        updated_content += '# Past Events\n\n'
-        updated_content += ''.join(f"## {event['name']}\n\nDate: {event['date'].strftime('%Y-%m-%d')}\nDescription: {event['description']}\nChannel: {event['channel']}\n\n" for event in existing_events if event['section'] == 'past')
+# Function to merge new events with existing ones and generate markdown content
+def merge_and_generate_markdown(frontmatter, existing_events, new_events):
+    # Remove past events that are now outdated
+    existing_events = [event for event in existing_events if event['section'] == 'upcoming' or event['date'] >= datetime.now().date()]
+    # Merge new events with existing events
+    for new_event in new_events:
+        if new_event not in existing_events:
+            existing_events.append(new_event)
+    # Sort events by date
+    existing_events.sort(key=lambda x: x['date'])
+    # Generate updated markdown content
+    upcoming_events_md = '\n\n'.join(
+        f"## {event['name']}\n\nDate: {event['date'].strftime('%b %d, %Y')}\n\nDescription:\n{event['description']}\n\nLocation: {event['channel_name']}"
+        for event in existing_events if event['section'] == 'upcoming')
+    past_events_md = '\n\n'.join(
+        f"## {event['name']}\n\nDate: {event['date'].strftime('%b %d, %Y')}\n\nDescription:\n{event['description']}\n\nLocation: {event['channel_name']}"
+        for event in existing_events if event['section'] == 'past')
+    
+    # Debug print
+    print("Upcoming Events Markdown:")
+    print(upcoming_events_md)
+    print("Past Events Markdown:")
+    print(past_events_md)
 
-        # Update the file on GitHub
-        repo.update_file(contents.path, "Updated events list", updated_content, contents.sha, branch="main")
+    # Combine the updated frontmatter with the updated event content
+    updated_content = f"---\n{frontmatter}---\n\n# Upcoming Events\n\n{upcoming_events_md}\n\n# Past Events\n\n{past_events_md}\n"
+    return updated_content
+
+
+# Main function to run the script
+def main():
+    events = fetch_discord_events(GUILD_ID, BOT_TOKEN)
+    print(events)
+    try:
+        with open(FILE_PATH, 'r', encoding='utf-8') as file:
+            content = file.read()
+        frontmatter, existing_events = parse_markdown(content)
+        updated_content = merge_and_generate_markdown(frontmatter, existing_events, events)
+        with open(FILE_PATH, 'w', encoding='utf-8') as file:
+            file.write(updated_content)
         print("The events.md file has been updated successfully.")
-    except GithubException as e:
-        print(f"An error occurred while updating the events.md file: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
+if __name__ == '__main__':
+    main()
 
-@client.event
-async def on_message(message):
-    # Check if the message is the command to update events and if the author is authorized
-    if message.content.startswith('!get_events') and message.channel.permissions_for(message.author).administrator:
-        # Fetch events from Discord here
-        # This is placeholder data; you should replace it with actual event fetching logic
-        events = [
-            {"name": "Event 3", "date": datetime(2023, 10, 30).date(), "description": "Description for Event 3", "channel": "general"},
-            {"name": "Event 4", "date": datetime(2023, 11, 5).date(), "description": "Description for Event 4", "channel": "events"}
-        ]
+# # Function to authenticate with GitHub
+# def authenticate_with_github(GH_PAT):
+#     return Github(GH_PAT)
 
-        update_markdown(events)
+# # Function to get file from GitHub
+# def get_github_file(REPO, FILE_PATH):
+#     contents = repo.get_contents(file_path, ref="main")
+#     return contents.decoded_content.decode('utf-8'), contents.sha
 
-        # Send a confirmation message
-        response = "Events have been updated."
-        await message.channel.send(response)
-
-client.run(TOKEN)
+# # Function to update file on GitHub
+# def update_github_file(REPO, FILE_PATH, content, sha, commit_message="Update events"):
+#     repo.update_file(FILE_PATH, commit_message, content, sha, branch="main")
