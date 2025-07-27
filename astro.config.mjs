a8b869af3abf57aa92c700f0877a9f041de7bae9
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2025-07-26 20:55:44 -0700

    use sharp instead of squoosh img for future compat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
@@ -56,7 +56,7 @@ export default defineConfig({
     Logger: 1
   }), tasks()],
   image: {
-    service: squooshImageService()
+    service: sharpImageService()
   },
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
